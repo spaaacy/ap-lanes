e4f583e9ba36f88bee7ca2341eb23e94a391101f
Aakif Ahamath <aakifahamath@gmail.com>
2023-04-21 22:03:23 +0800

    Added cancel button

--- lib/ui/passenger/components/journey_detail.dart
+++ lib/ui/passenger/components/journey_detail.dart
@@ -6,6 +6,7 @@ import '../../../data/model/firestore/journey.dart';
 class JourneyDetail extends StatefulWidget {
   final bool isSearching;
   final bool hasDriver;
+  final bool isPickedUp;
   final QueryDocumentSnapshot<Journey>? journey;
   final List<String> journeyDetails;
 
@@ -13,6 +14,7 @@ class JourneyDetail extends StatefulWidget {
     super.key,
     required this.isSearching,
     required this.hasDriver,
+    required this.isPickedUp,
     required this.journey,
     required this.journeyDetails,
   });
@@ -32,26 +34,54 @@ class _JourneyDetailState extends State<JourneyDetail> {
           child: AnimatedOpacity(
             opacity: (widget.journeyDetails.isNotEmpty) ? 1.0 : 0.0,
             duration: const Duration(milliseconds: 200),
-            child: Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(8),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.black54,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    padding: const EdgeInsets.all(16.0),
+                    itemCount: widget.journeyDetails.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Text(
+                        widget.journeyDetails[index],
+                        style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.white),
+                      );
+                    },
+                  ),
                 ),
-              ),
-              child: ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                padding: const EdgeInsets.all(16.0),
-                itemCount: widget.journeyDetails.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Text(
-                    widget.journeyDetails[index],
-                    style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.white),
-                  );
-                },
-              ),
+
+                SizedBox(height: 4.0,),
+
+                ...?(() {
+                  if (widget.hasDriver && !widget.isPickedUp) {
+                    return [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red
+                        ),
+                        onPressed: () {
+                          // TODO: Call journey cancel
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                          child: Text("Cancel"),
+                        ),
+                      )
+                    ];
+                  }
+                }())
+
+              ],
             ),
           ),
         ),
