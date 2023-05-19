2cef842a616d36b8a97b47aabc4fab8ca2ca40e8
Aakif Ahamath <aakifahamath@gmail.com>
2023-05-20 07:58:30 +0800

    Fixed login screen expanded bug

--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -1,11 +1,9 @@
-import 'package:ap_lanes/services/notification_service.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../services/auth_service.dart';
 import '../../util/constants.dart';
-import 'signup_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
@@ -22,72 +20,68 @@ class LoginScreen extends StatelessWidget {
         key: _signInFormKey,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(hintText: "Email"),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(hintText: "Email"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
 
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: const InputDecoration(hintText: "Password"),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
+                  return null;
+                },
+              ),
+              TextFormField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(hintText: "Password"),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
 
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 8.0),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      String email = emailController.text.trim();
-                      String password = passwordController.text.trim();
+                  return null;
+                },
+              ),
+              const SizedBox(height: 8.0),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    String email = emailController.text.trim();
+                    String password = passwordController.text.trim();
 
-                      if (_signInFormKey.currentState!.validate()) {
-                        String result = await context
-                            .read<AuthService>()
-                            .signIn(email: email, password: password);
-                        if (context.mounted) {
-                          if (result == signedIn) {
-                            Navigator.of(context).pop();
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text(result),
-                              ),
-                            );
-                          }
+                    if (_signInFormKey.currentState!.validate()) {
+                      String result = await context.read<AuthService>().signIn(email: email, password: password);
+                      if (context.mounted) {
+                        if (result == signedIn) {
+                          Navigator.of(context).pop();
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text(result),
+                            ),
+                          );
                         }
                       }
-                    },
-                    child: const Text("Login"),
-                  ),
+                    }
+                  },
+                  child: const Text("Login"),
                 ),
-                if (context.watch<firebase_auth.User?>() != null)
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: TextButton(
-                        onPressed: () {
-                          context.read<AuthService>().sendEmailVerification();
-                        },
-                        child: const Text("Send verification email")),
-                  )
-              ],
-            ),
+              ),
+              if (context.watch<firebase_auth.User?>() != null)
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: TextButton(
+                      onPressed: () {
+                        context.read<AuthService>().sendEmailVerification();
+                      },
+                      child: const Text("Send verification email")),
+                )
+            ],
           ),
         ),
       ),
