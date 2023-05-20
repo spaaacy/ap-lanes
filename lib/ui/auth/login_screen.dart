a2eaea85111551dc968457689901bfe33cc63f5a
Aakif Ahamath <aakifahamath@gmail.com>
2023-05-20 08:21:36 +0800

    Added initial text for email if email unverified

--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -14,6 +14,13 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authService = context.read<AuthService>();
+    final firebaseUser = context.read<firebase_auth.User?>();
+
+    if (firebaseUser?.email != null) {
+      emailController.text = firebaseUser!.email!;
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text("Login")),
       body: Form(
@@ -55,7 +62,7 @@ class LoginScreen extends StatelessWidget {
                     String password = passwordController.text.trim();
 
                     if (_signInFormKey.currentState!.validate()) {
-                      String result = await context.read<AuthService>().signIn(email: email, password: password);
+                      String result = await authService.signIn(email: email, password: password);
                       if (context.mounted) {
                         if (result == signedIn) {
                           Navigator.of(context).pop();
@@ -77,7 +84,7 @@ class LoginScreen extends StatelessWidget {
                   alignment: Alignment.bottomRight,
                   child: TextButton(
                       onPressed: () {
-                        context.read<AuthService>().sendEmailVerification();
+                        authService.sendEmailVerification();
                       },
                       child: const Text("Send verification email")),
                 )
