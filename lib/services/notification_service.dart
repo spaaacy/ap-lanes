017d0f1424cb087282e6e84e4cea79027430f92d
Aakif Ahamath <aakifahamath@gmail.com>
2023-05-20 14:48:10 +0800

    Added context for notification to use for IOS foreground

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -20,7 +20,11 @@ class NotificationService {
 
   FlutterLocalNotificationsPlugin get notificationPlugin => _notificationPlugin;
 
-  Future<void> initialize() async {
+  late final BuildContext _context;
+
+  Future<void> initialize(BuildContext context) async {
+    _context = context;
+
     const androidInitialization = AndroidInitializationSettings('ic_stat_feature_graphic');
 
     final iOSInitialization = DarwinInitializationSettings(
@@ -60,22 +64,22 @@ class NotificationService {
   // For iOS, when app is in foreground
   void onDidReceiveLocalNotification(int id, String? title, String? body, String? payload) async {
     // display a dialog with the notification details, tap ok to go to another page
-    // showDialog(
-    //   context: context,
-    //   builder: (BuildContext context) => CupertinoAlertDialog(
-    //     title: (title != null) ? Text(title) : null,
-    //     content: (body != null) ? Text(body) : null,
-    //     actions: [
-    //       CupertinoDialogAction(
-    //         isDefaultAction: true,
-    //         child: const Text('Ok'),
-    //         onPressed: () async {
-    //           Navigator.of(context, rootNavigator: true).pop();
-    //         },
-    //       )
-    //     ],
-    //   ),
-    // );
+    showDialog(
+      context: _context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: (title != null) ? Text(title) : null,
+        content: (body != null) ? Text(body) : null,
+        actions: [
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: const Text('Ok'),
+            onPressed: () async {
+              Navigator.of(context, rootNavigator: true).pop();
+            },
+          )
+        ],
+      ),
+    );
   }
 
 }
